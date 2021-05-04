f871a0ba58243a677f7ba20bb68184058499de8a
Ale Gómez <60187741+aleg001@users.noreply.github.com>
2021-05-04 08:31:04 -0600

    Creación de archivos

---
+++ calculadora.s
@@ -0,0 +1,18 @@
+/*
+*----------------------------------------
+* Lab #8
+* ---------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* CC3054 - Organización de computadoras
+* y Assembler
+* ---------------------------------------
+* Creado por:
+* Alejandro Gómez y Paola Contreras
+* ---------------------------------------
+* Fecha: 04/05/21
+*----------------------------------------*/
+
+.text
+.align 2
+.global main
+.type main,%function
