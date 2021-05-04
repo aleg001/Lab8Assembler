04575c2d6f888813c04198500bbecc785de8d0f5
Ale Gómez <60187741+aleg001@users.noreply.github.com>
2021-05-04 08:31:58 -0600

    Encabezado

---
+++ operaciones.s
@@ -0,0 +1,14 @@
+/*
+*----------------------------------------
+* Lab #8
+* ---------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* CC3054 - Organización de computadoras
+* y Assembler
+* ---------------------------------------
+* Creado por:
+* Alejandro Gómez y Paola Contreras
+* ---------------------------------------
+* Fecha: 04/05/21
+* Archivo: operaciones.s
+*----------------------------------------*/
