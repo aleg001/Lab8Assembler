aab9fc392a577d039458abbb63fcfddad1e444a8
Gabriela Paola Contreras Guerra <paolacontreras231@gmail.com>
2021-05-06 22:49:08 -0600

    Cambios

--- operaciones.s
+++ operaciones.s
@@ -10,14 +10,18 @@
 @Parametros de salida
 @r0: resultado
 /**********************************************************************/
+
+
 .global suma
 
 /* Se define la subrutina correspondiente al proceso de suma */
 suma:
+
 	add r4,r1,r0
 	mov r0,r4
 	mov pc,lr
-	
+
+
 /* Se define la subrutina correspondiente al proceso de multiplicacion */
 
 .global multiplicacion
@@ -25,51 +29,40 @@ multiplicacion:
 	mul r4,r1,r0
 	mov r0,r4
 	mov pc,lr
+
+
 	
 /* Se define la subrutina correspondiente al proceso de modulo*/
 .global modulo
 modulo:
-	mov r2,#0
-	cmp r1,r0
-	sub r1,r0
-	add r2,#1
-	mov pc, lr
-	b modulo
-
-
-.global modulopruebas
-
-modulopruebas:
-
-/* TODAVIA NO ESTA PROBADO
-	Basado en division hecha en clase.
-	Modificada para realizar procesos */
-	
+		mov r7,#0 /*contador*/
+		mov r8,r0
 	division:
-		cmp r0, r1
+		cmp r0,r1
 		bgt salirDiv
-		sub r7, r1
-		add r8,#1
+		sub r1,r0
+		add r7,#1
 		b division
 
 	salirDiv:
-		mul r1, r1, r8
-		mov r1, r9
-		sub r1, r1
-		mov r7, r1
-		bl printf
-		mov pc, r7
+		mul r0,r0,r7
+		mov r6,r8
+		sub r6,r1
+		mov r0,r6
 
 	
-.global potencia
-
 /* Se define la subrutina correspondiente al proceso de potenciar */
+
+.global potencia
 potencia:
-	mul r4,r0,r0
-	mul r5,r1,r1
-	add r0,r4,r5
-	mov pc,lr
+	eleva:
+		mul r1,r1,r1
+		sub r0,#1
+		cmp r0,#0
+		beq saleleva
+		b eleva
+	saleleva:
+		mov r0,r1
+		mov pc,lr
 
-
-.global concatenar
-concatenar:
+		
